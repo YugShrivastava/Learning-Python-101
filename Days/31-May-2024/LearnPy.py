a9b580b12f84a07e53d135b6bb2f9bf8296d1643
Yug <shrivastavayug04@gmail.com>
2024-05-31 21:14:38 +0530

    Day 1.

---
+++ Days/31-May-2024/LearnPy.py
@@ -0,0 +1,38 @@
+#Lesson 1: print("Hello, World!")
+
+# x = 1
+# if x == 1:
+#     # indented four spaces
+#     print("x is 1.")
+
+
+# Lesson 2: Data Types
+
+# myFloat  = 7.0
+# print(myFloat)
+
+# myFloat = float(7)
+# print(myFloat)
+
+# myString = 'hello'
+# print(myString)
+
+# myString = "hello"
+# print(myString)
+
+# mystring = "Don't worry about apostrophes"
+# print(mystring)
+
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
+
+# hello = "hello"
+# world = "world"
+# helloworld = hello + " " + world
+# print(helloworld)
+
+
+# Lesson 3: Lists
+
